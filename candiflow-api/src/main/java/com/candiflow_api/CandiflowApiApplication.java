3671b95ff4160f39e0b33059a6cc44cd30471944
alarboulletmarin <andrea,larboullet---marin@etu.unice.fr>
2025-04-27 09:31:17 +0200

    feat(backend): :gear: Configure initial backend

---
+++ candiflow-api/src/main/java/com/candiflow_api/CandiflowApiApplication.java
@@ -0,0 +1,13 @@
+package com.candiflow_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandiflowApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CandiflowApiApplication.class, args);
+    }
+
+}
---
+++ candiflow-api/src/test/java/com/candiflow_api/CandiflowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.candiflow_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandiflowApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
